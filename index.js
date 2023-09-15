67b10516391de68006ddf2fa7856796235d41a45
Lokesh Kumar <neolokeshkr@gmail.com>
2023-09-16 01:02:55 +0530

    feat: Add navbar and pricing refactor: Add browser-syncr npm script

--- index.js
+++ index.js
@@ -7,5 +7,3 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => console.log(`App listening @ port ${port}`))
-
-// D:\Lokesh\Develpoment\Challanges\NextGrowthLabs\npm-implementation\index.html
